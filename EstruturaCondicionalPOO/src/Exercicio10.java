7599264b68939437285ba337c8c43fecc4a3d86e
Vitória Caroline <vitoriacnf5578@gmail.com>
2022-09-03 19:52:56 -0300

    arquivos modificados

--- EstruturaCondicionalPOO/src/Exercicio10.java
+++ EstruturaCondicionalPOO/src/Exercicio10.java
@@ -1,5 +1,32 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Exercicio10 {
     public static void main(String[] args) {
-        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int numero1, numero2;
+
+        System.out.println("Digite dois números inteiros: ");
+        numero1 = sc.nextInt();
+        numero2 = sc.nextInt();
+
+        if (numero1 >= numero2) {
+            if (numero1 % numero2 == 0) {
+                System.out.print("São multiplos");
+            } else {
+                System.out.print("Não são multiplos");
+            }
+        } else {
+            if (numero2 % numero1 == 0) {
+                System.out.print("São multiplos");
+            } else {
+                System.out.print("Não são multiplos");
+            }
+        }
+
+        sc.close();
+
     }
 }
--- EstruturaCondicionalPOO/src/Exercicio13.java
+++ EstruturaCondicionalPOO/src/Exercicio13.java
@@ -3,18 +3,7 @@ import java.util.Scanner;
 
 public class Exercicio13 {
     public static void main(String[] args) {
-        Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
-        
-        int numero1, numero2;
-
-        System.out.println("Digite dois números inteiros: ");
-        numero1 = sc.nextInt();
-        numero2 = sc.nextInt();
-
-
-        sc.close();
-
+       
 
     }
 }
