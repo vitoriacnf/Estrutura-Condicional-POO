eda6e8da53e0e748648342a8868c14721e9d1e84
Vitória Caroline <vitoriacnf5578@gmail.com>
2022-09-03 19:34:24 -0300

    adicionando exercicio 12

--- EstruturaCondicionalPOO/src/Exercicio12.java
+++ EstruturaCondicionalPOO/src/Exercicio12.java
@@ -4,23 +4,22 @@ public class Exercicio12 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int horaInicial, horaFinal, duracaoJogo, horaTotal;
-        horaTotal = 24;
+        int horaInicial, horaFinal, duracaoJogo = 0;
 
         System.out.print("Hora Inicial: ");
         horaInicial = sc.nextInt();
         System.out.print("Hora Final: ");
         horaFinal = sc.nextInt();
 
-        if(horaInicial == 0 && horaFinal == 0){
-            System.out.println("Duração do jogo: " + horaTotal);
-
+        if(horaFinal > horaInicial){
+            //mesmo dia
+            duracaoJogo = horaFinal - horaInicial;    
+        }else {
+            //virou o dia
+            duracaoJogo = 24 - horaInicial + horaFinal;
         }
-        
-        horaTotal = horaInicial - horaFinal;
-        duracaoJogo = horaTotal;
-        System.out.printf("DURAÇÃO DO JOGO: " + duracaoJogo);
 
+        System.out.println("duracao do jogo e: " + duracaoJogo);
         sc.close();
     }
 }
