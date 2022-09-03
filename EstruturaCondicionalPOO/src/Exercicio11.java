da6d923c2c4bacb14a9fcb2a42dc11eac39a9350
Vitória Caroline <vitoriacnf5578@gmail.com>
2022-09-03 20:10:19 -0300

    exercicio 11 modificado

--- EstruturaCondicionalPOO/src/Exercicio11.java
+++ EstruturaCondicionalPOO/src/Exercicio11.java
@@ -6,36 +6,31 @@ public class Exercicio11 {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        double salario, novoSalario, aumento;
+        double salario, novoSalario = 0, aumento = 0;
+        int porcentagem;
 
         System.out.print("Digite o salário: R$ ");
         salario = sc.nextDouble();
 
         if (salario < 1000) {
             novoSalario = salario + (salario * 0.2);
-
+            porcentagem = 20;
         } else if (salario >= 1000 && salario <= 3000) {
             novoSalario = salario + (salario * 0.15);
-        
-        } else if (salario > 3000 && salario <= 8000) {
+            porcentagem = 15;
+        } else if (salario > 3000 && salario < 8000) {
             novoSalario = salario + (salario * 0.10);
-             
-        } else 
+            porcentagem = 10;
+        } else {
             novoSalario = salario + (salario * 0.05);
-        
+            porcentagem = 5;
+        }  
         
         System.out.printf("Novo salário: R$ %.2f%n", novoSalario);
         aumento = novoSalario - salario;
         System.out.printf("Aumento: R$ %.2f%n", aumento);
-        //System.out.print("Porcentagem: "+ porcentagem);
+        System.out.print("Porcentagem: "+ porcentagem + " % ");
 
         sc.close(); 
-        
-
-        }  
     }
-       
-
-
-
-
+}
