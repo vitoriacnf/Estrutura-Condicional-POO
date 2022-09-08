c35df5d26fcc89627e8a3fc5572000f3c92cbfac
Vitória Caroline <vitoriacnf5578@gmail.com>
2022-09-08 16:51:08 -0300

    modificação da questão 13

--- EstruturaCondicionalPOO/src/Exercicio13.java
+++ EstruturaCondicionalPOO/src/Exercicio13.java
@@ -3,7 +3,29 @@ import java.util.Scanner;
 
 public class Exercicio13 {
     public static void main(String[] args) {
-       
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
+        double X, Y;
+        X = sc.nextDouble();
+        Y = sc.nextDouble();
+
+        if (X == 0 && Y == 0) {
+            System.out.println("Origem");
+        } else if (X == 0 && Y != 0) {
+            System.out.println("Eixo Y");
+        } else if (X != 0 && Y == 0) {
+            System.out.println("Eixo X");
+        } else if (X > 0 && Y > 0) {
+            System.out.println("Q1");
+        } else if (X < 0 && Y > 0) {
+            System.out.println("Q2");
+        } else if (X < 0 && Y < 0) {
+            System.out.println("Q3");
+        } else if (X > 0 && Y < 0) {
+            System.out.println("Q4");
+        }
+        sc.close();
     }
+
 }
